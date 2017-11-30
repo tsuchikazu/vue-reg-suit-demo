219ca537e056821e7838de30893cf25e14c19bfa
tsuchikazu <huneyhunt77@gmail.com>
2017-11-30 23:27:46 +0900

    attach to document by vue-test-utils

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
+import { mount } from 'vue-test-utils'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    const wrapper = mount(HelloWorld, {
+      attachToDocument: true
+    })
+    expect(wrapper.find('.hello h1').text())
     .to.equal('Welcome to Your Vue.js App')
   })
 })
