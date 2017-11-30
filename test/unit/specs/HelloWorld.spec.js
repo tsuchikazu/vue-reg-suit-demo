7829cdf2d3fd1351124eb95891c30f19964dd8b7
tsuchikazu <huneyhunt77@gmail.com>
2017-11-30 23:40:53 +0900

    snapshot by karma-nightmare

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,12 +1,15 @@
 import { mount } from 'vue-test-utils'
 import HelloWorld from '@/components/HelloWorld'
+import { screenshot } from 'karma-nightmare'
 
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
+  it('should render correct contents', (done) => {
     const wrapper = mount(HelloWorld, {
       attachToDocument: true
     })
     expect(wrapper.find('.hello h1').text())
     .to.equal('Welcome to Your Vue.js App')
+
+    screenshot('./snapshot/HelloWorld.png').then(done)
   })
 })
